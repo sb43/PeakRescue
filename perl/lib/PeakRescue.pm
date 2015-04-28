9a2a0b53c80f6750f56d887f3418655be4532aa0
Shriram Bhosle <Shriram Bhosle sb43@sanger.ac.uk>
2015-04-28 10:20:11 +0100

    updated version

--- perl/lib/PeakRescue.pm
+++ perl/lib/PeakRescue.pm
@@ -7,7 +7,7 @@ our $VERSION = '1.0.0';
 our @EXPORT = qw($VERSION);
 const my $LICENSE =>
 "#################
-# PeakRescue::GlobalTranscript version %s, Copyright (C) 2015 Wellcome Trust Cancer Genome Project (CGP)
+# PeakRescue::GlobalTranscript version %s, Copyright (C) 2015 University of Edinburgh and Wellcome Trust Cancer Genome Project (CGP)
 # This scripts comes with ABSOLUTELY NO WARRANTY
 # See LICENSE for full details.
 #################";
